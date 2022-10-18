ee3fb4de712bd212e74571514c3c6fdf373e0dc4
hoanghy <hoanghy01122003@gmail.com>
2022-10-18 23:07:12 +0700

    update priority

--- src/components/task/task.mongo.js
+++ src/components/task/task.mongo.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 
 const TaskSchema = new mongoose.Schema({
-	title: { type: String, required: true },
+	title: { type: String, required: true, default: 'Untitled' },
 	location: { type: String, required: false },
+	priority: { type: Number, required: true, default: 3 },
 	time: {
 		from: { type: Date, required: true },
 		to: { type: Date, required: true },
