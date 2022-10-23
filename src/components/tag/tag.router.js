fe94770953f0ba6512dcec53d36933a6d7bcb636
hoanghy <hoanghy01122003@gmail.com>
2022-10-23 12:12:26 +0700

    update tag API

--- src/components/tag/tag.router.js
+++ src/components/tag/tag.router.js
@@ -12,8 +12,8 @@ import { authorizeRouteMiddleware } from '../../middleware/authentication'
 const tagRouter = express.Router()
 
 tagRouter.post('/', authorizeRouteMiddleware, httpCreateNewTag)
-tagRouter.get('/all', authorizeRouteMiddleware, httpGetAllTags)
-tagRouter.get('/', authorizeRouteMiddleware, httpGetTagByTitle)
+tagRouter.get('/', authorizeRouteMiddleware, httpGetAllTags)
+tagRouter.get('/find', authorizeRouteMiddleware, httpGetTagByTitle)
 tagRouter.put('/:tagID', authorizeRouteMiddleware, httpUpdateTag)
 tagRouter.delete('/:tagID', authorizeRouteMiddleware, httpRemoveTag)
 export default tagRouter
