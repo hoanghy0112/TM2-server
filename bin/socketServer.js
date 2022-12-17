6869fbc8bbe1d0643f64705d7ea8506695da9ddc
Hoang Hy <hoanghy01122003@gmail.com>
2022-12-18 04:38:20 +0700

    update cors

--- bin/socketServer.js
+++ bin/socketServer.js
@@ -1,6 +1,12 @@
 import { Server } from 'socket.io'
 import server from './httpServer'
 
-const io = new Server(server, { path: '/socket/v2/' })
+const io = new Server(server, {
+	path: '/socket/v2/',
+	cors: {
+		credentials: true,
+		origin: true,
+	},
+})
 
 export default io
